5630f6156d7db031b37a402a21486153a7b2124c
Jônatas Torquato <jonatas.alfatorquato@gmail.com>
2024-12-08 20:28:18 -0300

    RL0Q2

--- Estrutura de Dados e Algoritmos/Lista000/RL0Q2.c
+++ Estrutura de Dados e Algoritmos/Lista000/RL0Q2.c
@@ -1,113 +1,264 @@
-// CÓDIGO AINDA EM DESENVOLVIMENTO //
-
-#define ERROR -1
-#define MAX 1000
-#define TAM 1000
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 #include <math.h>
 
-typedef struct Points {
-    int x;
-    int y;
-} Points;
+#define erro -1
+#define tamanho 1000 /* Tamanho máximo para os arrays */
+#define maximo 1000 /* Tamanho máximo de dados da linha */
 
-typedef struct Strings {
-    char string[TAM];
-} Strings;
+#define palavra -2
+#define pontoFlutuante -3
+#define inteiro -4
+
+typedef struct String{
+    char string[tamanho];
+} String;
+
+typedef struct Point {
+    int pontoX, pontoY;
+} Point;
+
+typedef struct PointFloat {
+    float pontoXf, pontoYf;
+} PointFloat;
 
 typedef struct Inteiro {
-    int nInt;
+    int iNumero;
 } Inteiro;
 
-typedef struct Floats {
-    int nFloat;
-} Floats;
+typedef struct Float {
+    float fNumero;
+} Float;
 
-typedef struct Line {
-    Strings listaString[TAM];
-    Inteiro nInt[TAM];
-    Floats nFloat[TAM];
-    Points points[TAM];
-} Line;
+// J - Adicionei 2 funções
 
-int ehString(char *string){
-    int i = 0;
-    int cont = 0;
+// J - Calcula a distância entre 2 pontos da coordenada
+double distancia2Pontos(float x1, float y1, float x2, float y2) {
+    float tempX;
+    float tempY;
+    double temp;
+    tempX = (x2 - x1) * (x2 - x1);
+    tempY = (y2 - y1) * (y2 - y1);
+    temp = sqrt(tempX + tempY);
 
-    while(((string[i] >= 'a') && (string[i] <= 'z')) || ((string[i] >= 'A') && (string[i] <= 'Z'))){
-        cont++;
-    }
-    
-    if(cont>0){
-        return 1;
-    } else{
-        return 0;
-    }
+    return temp;
 }
 
-int ehInteiro(char *string){
-    int i = 0;
-    int cont = 0;
-
-    while((string[i] >= 0) && (string[i] <= 9)){
-        cont++;
+// J - Limpa o slice caso haja '\n' e coloca '\0'
+void limparString(char *str) {
+    char *p = strchr(str, '\n'); // Procura o caractere '\n'
+    if (p) {
+        *p = '\0'; // Substitui '\n' por '\0'
     }
-    
-    if(cont>0){
-        return 1;
-    } else{
-        return 0;
+} 
+
+int verificarDado(char buffer[], String listaString[], String listaStringOrdenada[], Float listaFloat[], Float listaFloatOrdenada[], Inteiro listaInt[], Inteiro listaIntOrdenada[], int *quantidadeChar, int *quantidadeFloat, int *quantidadeInt){
+    /* Verificar se o dado é uma palavra */
+    bool isPalavra = false;
+    for (int icont = 0; icont < strlen(buffer); icont = icont + 1){
+        if (isalpha(buffer[icont])){
+            isPalavra = true;
+            break;
+        }
     }
-}
 
-int verificaString(char *string){
-    int i = 0;
-    int j = 0;
-    char stringAux[MAX];
-
-    while(((string[i] >= 'a') && (string[i] <= 'z')) || ((string[i] >= 'A') && (string[i] <= 'Z'))){
-        stringAux[j] = string[i];
-        j++;
+    if (isPalavra == true){
+        limparString(buffer);
+        strcpy(listaString[*quantidadeChar].string, buffer);
+        strcpy(listaStringOrdenada[*quantidadeChar].string, buffer);
+        *quantidadeChar = *quantidadeChar + 1;
+        return palavra;
     }
-    stringAux[j] = '\0';
-}
 
-int main(){
-    Line listaValores[MAX];
+    /* Verificar se o dado é um ponto flutuante */
+    bool isFloat = false;
+    int pontoCount = 0;
+    for (int icont = 0; icont < strlen(buffer); icont = icont + 1){
+        if (buffer[icont] == '.'){
+            pontoCount = pontoCount + 1;
+        }
 
-    int qtdeValores = 0;
+        if (isdigit(buffer[icont]) || buffer[icont] == '.'){
+            isFloat = true;
+        } else { isFloat = false; break; }
+    }
 
-    FILE *fp_in = fopen("L0Q1.in", "r");
-    FILE *fp_out = fopen("L0Q1.out", "w");
+    if (isFloat == true && pontoCount == 1){
+        listaFloat[*quantidadeFloat].fNumero = atof(buffer);
+        listaFloatOrdenada[*quantidadeFloat].fNumero = atof(buffer);
+        *quantidadeFloat = *quantidadeFloat + 1;
+        return pontoFlutuante;
+    } else {
+
+        /* Verificar se o dado é um inteiro */
+        for (int icont = 0; icont < strlen(buffer); icont = icont + 1){
+            if (isdigit(buffer[icont])){
+                listaInt[*quantidadeInt].iNumero = atoi(buffer);
+                listaIntOrdenada[*quantidadeInt].iNumero = atoi(buffer);
+                *quantidadeInt = *quantidadeInt + 1;
+                return inteiro;
+            }
+        }
+    }
+} 
+
+int main (){
+    Point listaPonto[tamanho], listaOrdenada[tamanho];
+    PointFloat flistaPonto[tamanho], flistaOrdenada[tamanho];
+    Inteiro listaInt[tamanho], listaIntOrdenada[tamanho];
+    Float listaFloat[tamanho], listaFloatOrdenada[tamanho];
+    String listaString[tamanho], listaStringOrdenada[tamanho];
+
+    int retorno;
+    char buffer[maximo];
+
+    int quantidadeInt = 0, quantidadeFloat = 0, quantidadePontos = 0, quantidadeChar = 0, fquantidadePontos = 0;
+
+    FILE *fp_in = fopen("L0Q2.in", "r");
+    FILE *fp_out = fopen("L0Q2.out", "w");
 
     if (fp_in == NULL || fp_out == NULL) {
         printf("Files cannot be opened\n");
-        return ERROR;
+        return erro;
     }
 
-    char line[TAM];
+    char line[maximo]; /* Capacitade de caractere máximo da linha */
+    while (fgets(line, sizeof(line), fp_in)){
+        quantidadeInt = quantidadeFloat = quantidadePontos = quantidadeChar = 0, fquantidadePontos = 0; /* Reiniciar a contagem para cada linha */
+        
+        /* Ler cada dados do arquivo de entrada */
+        char *slice = strtok(line, " "); /* Separador padrão de cada dado */
 
-    while (fgets(line, sizeof(line), fp_in)) {
-        qtdeValores = 0; // Resetar a contagem para cada linha
-
-        // Ler cada ponto
-        char *slice = strtok(line, " ");
-        slice = strtok(NULL, " ");  // Ignorar "points"
-
-        while (slice != NULL) {
-            int x, y;
-            sscanf(slice, "(%d,%d)", &x, &y);
-            if (qtdePontos < MAX) {
-                listaPontos[qtdePontos].x = x;
-                listaPontos[qtdePontos].y = y;
-                listaOrdenada[qtdePontos] = listaPontos[qtdePontos]; // Cópia para ordenação
-                qtdePontos++;
+        while (slice != NULL){
+            int iX, iY;
+            float fX, fY;
+            if (sscanf(slice, " (%d,%d)", &iX, &iY) == 2){
+                if(quantidadePontos < tamanho){
+                    listaPonto[quantidadePontos].pontoX = iX;
+                    listaPonto[quantidadePontos].pontoY = iY;
+                    listaOrdenada[quantidadePontos] = listaPonto[quantidadePontos]; /* Cópia para ordenar a lista de pontos na função */
+                    quantidadePontos = quantidadePontos + 1;
+                }
+            } else if (sscanf(slice, " (%f,%f)", &fX, &fY) == 2) {
+                if(fquantidadePontos < tamanho){
+                    flistaPonto[fquantidadePontos].pontoXf = fX;
+                    flistaPonto[fquantidadePontos].pontoYf = fY;
+                    flistaOrdenada[fquantidadePontos] = flistaPonto[fquantidadePontos]; /* Cópia para ordenar a lista de pontos na função */
+                    fquantidadePontos = fquantidadePontos + 1;
+                }
+            } else {
+                retorno = verificarDado(slice, listaString, listaStringOrdenada, listaFloat, listaFloatOrdenada, listaInt, listaIntOrdenada, &quantidadeChar, &quantidadeFloat, &quantidadeInt);
             }
             slice = strtok(NULL, " ");
         }
+        
+        /* Ordenação dos ponto usando bubble sort */
+        for (int icont = 0; icont < quantidadePontos - 1; icont = icont + 1){
+            for (int jcont = 0; jcont < quantidadePontos - icont - 1; jcont = jcont + 1){
+                float d1 = distancia2Pontos(0, 0, listaOrdenada[jcont].pontoX, listaOrdenada[jcont].pontoY);
+                float d2 = distancia2Pontos(0, 0, listaOrdenada[jcont + 1].pontoX, listaOrdenada[jcont + 1].pontoY);
+                if (d1 > d2){
+                    Point aux = listaOrdenada[jcont];
+                    listaOrdenada[jcont] = listaOrdenada[jcont + 1];
+                    listaOrdenada[jcont + 1] = aux;
+                }
+            }
+        }
 
+        // J - Verifica a distância dos pontos considerando a origem (0,0) e realiza a ordenação
 
+        /* Ordenação dos ponto (float) usando bubble sort */
+        for (int icont = 0; icont < fquantidadePontos - 1; icont = icont + 1){
+            for (int jcont = 0; jcont < fquantidadePontos - icont - 1; jcont = jcont + 1){
+                float d1 = distancia2Pontos(0, 0, flistaOrdenada[jcont].pontoXf, flistaOrdenada[jcont].pontoYf);
+                float d2 = distancia2Pontos(0, 0, flistaOrdenada[jcont + 1].pontoXf, flistaOrdenada[jcont + 1].pontoYf);
+                if (d1 > d2){
+                    PointFloat aux = flistaOrdenada[jcont];
+                    flistaOrdenada[jcont] = flistaOrdenada[jcont + 1];
+                    flistaOrdenada[jcont + 1] = aux;
+                }
+            }
+        }
 
+        /* Ordenação da String usando bubble sort */
+        for (int icont = 0; icont < quantidadeChar - 1; icont = icont + 1) {
+            for (int jcont = 0; jcont < quantidadeChar - icont - 1; jcont = jcont + 1) {
+                bool troca = false;
+                for (int kcont = 0; kcont < tamanho; kcont = kcont + 1) {
+                    /* Se a string atual for maior que a próxima */
+                    if (listaStringOrdenada[jcont].string[kcont] > listaStringOrdenada[jcont + 1].string[kcont]) {
+                        troca = true;
+                        break;
+                    }
+                    /* Se a string atual for menor, não é necessário continuar a comparação */
+                    if (listaStringOrdenada[jcont].string[kcont] < listaStringOrdenada[jcont + 1].string[kcont]) {
+                        break;
+                    }
+                }
+                /* Troca as posições se necessário */
+                if (troca) {
+                    String aux = listaStringOrdenada[jcont];
+                    listaStringOrdenada[jcont] = listaStringOrdenada[jcont + 1];
+                    listaStringOrdenada[jcont + 1] = aux;
+                }
+            }
+        }
+
+        /* Ordenação do Float usando bubble sort */
+        for (int icont = 0; icont < quantidadeFloat - 1; icont = icont + 1){
+            for (int jcont = 0; jcont < quantidadeFloat - icont - 1; jcont = jcont + 1){
+                if (listaFloatOrdenada[jcont].fNumero > listaFloatOrdenada[jcont + 1].fNumero){
+                    Float aux = listaFloatOrdenada[jcont];
+                    listaFloatOrdenada[jcont] = listaFloatOrdenada[jcont + 1];
+                    listaFloatOrdenada[jcont + 1] = aux;
+                }
+            }
+        }
+        
+
+        /* Ordenação do Inteiro usando bubble sort */
+        for (int icont = 0; icont < quantidadeInt - 1; icont = icont + 1){
+            for (int jcont = 0; jcont < quantidadeInt - icont - 1; jcont = jcont + 1){
+                if (listaIntOrdenada[jcont].iNumero > listaIntOrdenada[jcont + 1].iNumero){
+                    Inteiro aux = listaIntOrdenada[jcont];
+                    listaIntOrdenada[jcont] = listaIntOrdenada[jcont + 1];
+                    listaIntOrdenada[jcont + 1] = aux;
+                }
+            }
+        }
+        
+
+        /* Escrever os dados ordenados no arquivo de saida */
+        fprintf(fp_out, "str:");
+        for (int icont = 0; icont < quantidadeChar; icont = icont + 1){
+            fprintf(fp_out, " %s", listaStringOrdenada[icont].string);
+        }
+        fprintf(fp_out, " int:");
+        for (int icont = 0; icont < quantidadeInt; icont = icont + 1){
+            fprintf(fp_out, " %d", listaIntOrdenada[icont].iNumero);
+        }
+        fprintf(fp_out, " float:");
+        for (int icont = 0; icont < quantidadeFloat; icont = icont + 1){
+            fprintf(fp_out, " %.2f", listaFloatOrdenada[icont].fNumero);
+        }
+        fprintf(fp_out, " p:");
+        if (fquantidadePontos > 0) { // Se houver pontos flutuantes
+            for (int icont = 0; icont < fquantidadePontos; icont = icont + 1) {
+                fprintf(fp_out, " (%.2f,%.2f)", flistaOrdenada[icont].pontoXf, flistaOrdenada[icont].pontoYf);
+            }
+        }
+        if (quantidadePontos > 0) { // Se houver pontos inteiros
+            for (int icont = 0; icont < quantidadePontos; icont = icont + 1) {
+                fprintf(fp_out, " (%d,%d)", listaOrdenada[icont].pontoX, listaOrdenada[icont].pontoY);
+            }
+        }
+        fprintf(fp_out, "\n");
+    }
+
+    fclose(fp_in);
+    fclose(fp_out);
+    return EXIT_SUCCESS;
 }
